82802023f35c6816053b0290968a6f608f36ddb6
Talha Hussain <talhahussain056@gmail.com>
2024-06-02 11:34:54 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:interactive_forms_with_rive/main_module.dart';
+
+import 'core/route/app_route.dart';
+import 'core/route/app_route_name.dart';
+import 'core/theme/app_theme.dart';
+
+
+void main() {
+  MainModule.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Interactive Login Form with Rive",
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.light,
+      initialRoute: AppRouteName.home,
+      onGenerateRoute: AppRoute.generate,
+    );
+  }
+}
